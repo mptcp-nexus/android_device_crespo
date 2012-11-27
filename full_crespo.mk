56f72b615b2bd2999dfdeeb617ae7c0e8dec2b39
Raphael Wutzke <raphael.wutzke@googlemail.com>
2012-11-27 13:33:44 +0100

    Add MPTCP support
    Exchange kernel with MPTCP kernel
    Include MPTCPControl app

--- full_crespo.mk
+++ full_crespo.mk
@@ -29,6 +29,10 @@ PRODUCT_PACKAGES += \
         VisualizationWallpapers \
         librs_jni
 
+# MPTCP
+PRODUCT_PACKAGES += \
+       MPTCPControl
+
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # This is where we'd set a backup provider if we had one
